c48db9d05ebde830b7b632995aaa51bf56440d12
Tara Sankhalpara <44140923+tsankhalpara@users.noreply.github.com>
2019-04-04 13:25:54 +0100

    Update docking_station_spec.rb
    updated with pairing session work with sejin

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,31 +2,38 @@ require 'docking_station'
 require 'bike'
 
 describe DockingStation do
+it {is_expected.to respond_to :release_bike}
 
-  it 'release_bike' do
+describe '#release_bike' do
+
+  it "releases a bike" do
     bike = Bike.new
     subject.dock(bike)
     expect(subject.release_bike).to eq bike
   end
 
-  it 'creates new bike object if bike is working' do
-
-    bike = Bike.new
-    expect(bike).to be_working
+  it "raises an error when no bikes are docked" do
+    expect{subject.release_bike}.to raise_error 'No bikes available'
   end
-
- it 'docks bike' do
- bike = Bike.new
- expect(subject.dock(bike)).to eq bike
 end
 
- it 'raises an error when there are no bikes available' do
- expect { subject.release_bike }.to raise_error 'No bikes available'
+describe '#dock' do
+  it "raises an error if the docking station is full" do
+    DockingStation::DEFAULT_CAPACITY.times {subject.dock(Bike.new)}
+    expect{subject.dock(Bike.new)}.to raise_error 'Docking station full'
   end
 
-  it 'raises an error when the docking station is full' do
-  20.times { subject.dock Bike.new}
-    expect { subject.dock Bike.new }.to raise_error 'Docking station is full'
-  end
+  #it 'docks a bike' do
+  #  subject.dock(bike)
+  #  expect(subject.dock(bike)).to eq @bike
+  #end
+end
+
+#it "releases a working bike" do
+#  bike = subject.release_bike
+#  expect(bike).to be_working
+#end
+
+it {is_expected.to respond_to(:dock).with(1).argument}
 
 end
