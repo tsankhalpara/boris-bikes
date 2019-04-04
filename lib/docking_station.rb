3b46d6f356dd9d8ef787d77fe9316d0e22063c65
Tara Sankhalpara <44140923+tsankhalpara@users.noreply.github.com>
2019-04-04 13:21:40 +0100

    Update docking_station.rb
    update this with work from pairing session with Sejin

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,20 +1,29 @@
 require_relative 'bike'
 
 class DockingStation
-  attr_reader :bike
-
+  DEFAULT_CAPACITY = 20
   def initialize
-    @bikes = []
+    @bike = []
   end
-
   def release_bike
-    fail 'No bikes available' if @bike.empty?
-    @bikes.pop
+    fail 'No bikes available' if empty?
+    @bike.pop
   end
 
   def dock(bike)
-    fail 'Docking station is full' if @bike.count >= 20
-    @bikes << bike
+    fail 'Docking station full' if full?
+    @bike << bike
   end
 
+  private
+
+  def full?
+    @bike.count >= DEFAULT_CAPACITY
+  end
+
+  def empty?
+    @bike.empty?
+  end
+
+  attr_reader :bike
 end
